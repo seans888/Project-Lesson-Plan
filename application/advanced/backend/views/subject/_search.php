eca383acdf3d38d4c69c591066eceafaa22944a4
Aaron Dagatan <aaroncdagatan@gmail.com>
2016-08-11 14:56:27 +0800

    added subject model and crud

---
+++ application/advanced/backend/views/subject/_search.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\SubjectSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="subject-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'sub_name') ?>
+
+    <?= $form->field($model, 'emp_id') ?>
+
+    <?= $form->field($model, 'sub_time') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
