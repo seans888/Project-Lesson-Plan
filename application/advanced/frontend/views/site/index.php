b089bf84ff80da70e54ad2558f2ff4422284a572
Neil Cueto <neilcueto101@gmail.com>
2016-08-18 16:24:57 +0800

    edit yii layout view forms

--- application/advanced/frontend/views/site/index.php
+++ application/advanced/frontend/views/site/index.php
@@ -7,41 +7,31 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>WELCOME TO VILLE ST. JOHN ACADEMY</h1>
+    <font color="efec1f">
+        <h1><b>WELCOME TO VILLE ST. JOHN ACADEMY</b></h1>
+        </font>
     </div>
 
     <div class="body-content">
-
+<font size="4">
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+            
+                
+                <p>Ville St. John Academy is a Catholic school owned and managed by the Sisters of St. John the Baptist. 
+                It is situated in the most tranquil end of Marcelo Green Village, Maharlika Avenue, Phase V, Parañaque City.
+                 It has the utmost conducive atmosphere free from the hassle of the city. It fosters the “Family Spirit” which 
+                 is clearly seen in the various activities set forth in and outside the school campus.
+                </p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p>The Baptistine Education apostolate first started in the small town of Angri, Salerno, South of Italy,
+                 by the Sisters of St. John the Baptist through the paternal guidance of its Founder, Blessed Father Father
+                  Alfonso with the purpose to educate and evangelize the orphan of war with gospel values and to liberate them
+                   from ignorance, exploitation so they wouldn’t become problems of society. This tiny seed has spread throughout 
+                   (17) seventeen countries namely, United States, Brazil, Zambia, Canada, Chile, Israel, Botswana, India, Poland, 
+                   Korea, Argentina, Philippines, Mexico, Albania, Romania, South Africa and ameron.</p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        
             </div>
         </div>
 
