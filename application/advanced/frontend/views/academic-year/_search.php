7e0461e0b53f665ff0e05f3476ae3f97bf428c74
Neil Cueto <neilcueto101@gmail.com>
2016-08-19 20:16:00 +0800

    added academic frontend controller

---
+++ application/advanced/frontend/views/academic-year/_search.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\AcademicYearSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="academic-year-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'acad_year_start') ?>
+
+    <?= $form->field($model, 'acad_year_end') ?>
+
+    <?= $form->field($model, 'quart1_start_period') ?>
+
+    <?= $form->field($model, 'quart1_end_period') ?>
+
+    <?php // echo $form->field($model, 'quart2_start_period') ?>
+
+    <?php // echo $form->field($model, 'quart2_end_period') ?>
+
+    <?php // echo $form->field($model, 'quart3_start_period') ?>
+
+    <?php // echo $form->field($model, 'quart3_end_period') ?>
+
+    <?php // echo $form->field($model, 'quart4_start_period') ?>
+
+    <?php // echo $form->field($model, 'quart14_end_period') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
