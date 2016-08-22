74ce9e508f93a1d64b429085c48b406f70fe1e71
Jonathan Abalon <abalonjonathan@gmail.com>
2016-08-22 14:30:04 +0800

    create front end Job CRUD

---
+++ application/advanced/frontend/views/job/index.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\JobSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Jobs';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="job-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Job', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'job_description',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
