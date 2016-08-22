75eb4589184ba39f9a544abc31d5a67f038f2997
Neil Cueto <neilcueto101@gmail.com>
2016-08-22 14:27:49 +0800

    modified backend job layout

--- application/advanced/backend/views/job/index.php
+++ application/advanced/backend/views/job/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+           // 'id',
             'job_description',
 
             ['class' => 'yii\grid\ActionColumn'],
