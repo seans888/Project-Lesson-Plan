82ad99323cb74a66e4272d2e836dbb924eb1f5c1
Neil Cueto <neilcueto101@gmail.com>
2016-10-22 22:35:20 +0800

    edit backend form

--- application/advanced/backend/views/employee/index.php
+++ application/advanced/backend/views/employee/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Employee', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Add Employee', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
