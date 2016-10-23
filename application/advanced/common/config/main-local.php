4cdf50dad043f2c546ac884dae96b5f035f49403
odabalon <abalonjonathan@gmail.com>
2016-10-23 15:05:47 +0800

    changed db name common config

--- application/advanced/common/config/main-local.php
+++ application/advanced/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=vsja',
+            'dsn' => 'mysql:host=localhost;dbname=registrar_system',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
